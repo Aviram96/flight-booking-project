6d72f4f6d4c912d69e923f812d91f4d837cb0896
Aviram_ <aviramhasuv@gmail.com>
2024-12-05 05:15:22 +0200

    Fix tests in app.component.spec.ts

--- flight-booking-client/src/app/app.component.spec.ts
+++ flight-booking-client/src/app/app.component.spec.ts
@@ -14,16 +14,23 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'flight-booking-client' title`, () => {
+  it('should have a method fetchFlights', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('flight-booking-client');
+    expect(app.fetchFlights).toBeDefined();
   });
 
-  it('should render title', () => {
+  it('should render available flights section', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, flight-booking-client');
+    expect(compiled.querySelector('h3')?.textContent).toContain('Available Flights');
+  });
+
+  it('should render my bookings section', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h3')?.textContent).toContain('My Bookings');
   });
 });
